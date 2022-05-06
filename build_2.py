6b3739de9b9354ddc2702b320834ce9749630b2c
molava <103011082+molava@users.noreply.github.com>
2022-05-06 13:52:56 -0700

    Update build_2.py
    removing ./ from files paths

--- build_2.py
+++ build_2.py
@@ -2,15 +2,15 @@
 def main(*complete_template):
 
 #Opening base template htmls 
-    top_template = open('./templates/top.html').read()
-    bottom_template = open('./templates/bottom.html').read()
+    top_template = open('templates/top.html').read()
+    bottom_template = open('templates/bottom.html').read()
     
     base = top_template + bottom_template
     open('base.html', 'w+').write(base)
   
 #replacing title and content in base template
-    template = open('./templates/base.html').read()
-    index_content = open('./content/index.html').read() 
+    template = open('templates/base.html').read()
+    index_content = open('content/index.html').read() 
      
     finished_index_page = template.replace( '{{content}}', index_content)
     open ('docs/index.html','w+').write(finished_index_page)
@@ -21,15 +21,15 @@ main()
 
 def content_pages():   
 
-    content = open('./content/index.html').read()
+    content = open('content/index.html').read()
     index_html = main(content) 
     open('index.html', 'w+').write(index_html)
     
-    about_me = open('./content/about_me.html').read()
+    about_me = open('content/about_me.html').read()
     about_me_html = main(about_me)
     open('about_me.html', 'w+').write(str(about_me_html))
 
-    contact_me = open('./content/contact_me.html').read()
+    contact_me = open('content/contact_me.html').read()
     contact_me_html = main(contact_me)
     open('contact_me.html', 'w+').write(str(contact_me_html))
     
@@ -40,17 +40,17 @@ content_pages()
 def growing_pages():
     pages = [
     {
-    "my_home_page": "./content/index.html",
+    "my_home_page": "content/index.html",
     "output": "docs/index.html",
     "title": "Molava's Page",
     },
     {    
-    "about_me": "./content/about_me.html",
+    "about_me": "content/about_me.html",
     "output": "docs/about_me.html",
     "title": "About Molava",
     },
     {
-    "contact_me": "./content/contact_me.html",
+    "contact_me": "content/contact_me.html",
     "output": "docs/contact_me.html",
     "title": "Ring Ring",
     },
